f809ef1d7c353be7808ae540304ae6ff743fd30e
Thomas Wright <twwright@gmail.com>
2019-10-19 19:46:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.count == 1
     array.join
-  elsif array.count = 2
+  elsif array.count == 2
     array.join(" and ")
   else
     oxford_arr = array[0..-2].join(", ")
