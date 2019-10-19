862216472fa169cc3e668a9702d8100475aa5c3c
Thomas Wright <twwright@gmail.com>
2019-10-19 19:45:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.count == 1
     array.join
+  elsif array.count = 2
+    array.join(" and ")
   else
     oxford_arr = array[0..-2].join(", ")
     oxford_arr + " and " + array.pop
