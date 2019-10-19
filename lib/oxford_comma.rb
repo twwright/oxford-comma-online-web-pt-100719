1cf725a549b87e4c2c2cc71d3eb61c684cf5eae2
Thomas Wright <twwright@gmail.com>
2019-10-19 19:31:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.count == 1
     array.join
   else
-    array[array.last] = "and #{array.last}"
+    array[array.last] = "and #{array[last]}"
     oxford_arr = array.join(", ")
     oxford_arr
   end
